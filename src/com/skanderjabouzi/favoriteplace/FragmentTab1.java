e30f0cef9238d60325f6c9b8b8530f15dac55aca
Skander Jabouzi <jabouzi@gmail.com>
2015-09-30 14:29:02 -0400

    add tabs for test

---
+++ src/com/skanderjabouzi/favoriteplace/FragmentTab1.java
@@ -0,0 +1,17 @@
+package com.skanderjabouzi.favoriteplace;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.app.Fragment;
+
+public class FragmentTab1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragmenttab1, container, false);
+        return rootView;
+    }
+ 
+}
