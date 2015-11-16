95b6e3f5e77734379a01f66ad92ce10deb535b29
Skander Jabouzi <jabouzi@gmail.com>
2015-11-16 10:39:03 -0500

    fix app to run on android

---
+++ app/src/main/java/com/skanderjabouzi/favoriteplace/AboutActivity.java
@@ -0,0 +1,15 @@
+package com.skanderjabouzi.favoriteplace;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+
+
+public class AboutActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
